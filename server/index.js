7d34fc9b64eb4932f1bcf7cb7931d6fed5018dbc
Juweria86 <jujumelmi143@gmail.com>
2024-09-17 11:37:36 +0300

    updated the file upload logic

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
+app.use('/uploads', express.static('uploads'));
 
 
 
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,6 @@
 import express from "express";
 import auth from "../middleware/auth.js";
+import upload from '../middleware/upload.js';
 
 const router = express.Router();
 
@@ -7,8 +8,8 @@ import { getPosts, createMemory, updatePost, deletePost } from '../controllers/p
 
 
 router.get('/getpost', getPosts);
-router.post('/create', auth, createMemory);
-router.patch('/:id/update', auth, updatePost);
-router.delete('/:id/delete', auth, deletePost);
+router.post('/create', auth, upload.single('selectedFile'), createMemory);
+router.patch('/update/:id', auth, upload.single('selectedFile'), updatePost);
+router.delete('/delete/:id', auth, deletePost);
 
 export default router;
