ec1f6f1141aaee832fe1c2fd800aed77655e1f1d
Juweria86 <jujumelmi143@gmail.com>
2024-09-18 12:00:36 +0300

    added the search and pagination functionalities

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -4,13 +4,17 @@ import upload from '../middleware/upload.js';
 
 const router = express.Router();
 
-import { getPosts, createMemory, updatePost, deletePost, likePost, commentPost } from '../controllers/posts.js';
+import { getPosts, createMemory, updatePost, deletePost, likePost, commentPost, getPostsByCreator, getPostsBySearch, getPostsByPage } from '../controllers/posts.js';
 
+router.get('/creator', getPostsByCreator);
+router.get('/search', getPostsBySearch);
+router.get('/', getPostsByPage);
+router.get('/:id', getPosts);
 
-router.get('/getpost', getPosts);
 router.post('/create', auth, upload.single('selectedFile'), createMemory);
 router.patch('/update/:id', auth, upload.single('selectedFile'), updatePost);
 router.delete('/delete/:id', auth, deletePost);
+
 router.patch('/likePost/:id', auth, likePost);
 router.post('/commentPost/:id', auth, commentPost);
 
