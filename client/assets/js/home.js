97384077a24ce179cb3abf1439c53f29cb2be6fb
Peace-lov <chidieberepeace42@gmail.com>
2024-09-21 12:43:12 +0100

    home page

---
+++ client/assets/js/home.js
@@ -0,0 +1,7 @@
+document.getElementById('getStartedBtn').addEventListener('click', function() {
+    alert('Redirecting to sign up or login page...');
+
+    //Redirecting to sign up or login page
+    window.location.href = "/client/pages/signup.html";
+  });
+  
